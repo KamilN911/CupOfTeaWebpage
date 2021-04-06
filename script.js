03e8c4685114dbdc8271c21b36ce303ce9beca2d
Kamil <kamilnowak911@gmail.com>
2021-04-06 15:29:12 +0200

    implementing credentials section

--- script.js
+++ script.js
@@ -3,69 +3,9 @@ const a2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla dapib
 const a3 = "Donec mi eros, auctor ac luctus tempus, tincidunt in sem. Aliquam erat volutpat. Cras ac sapien id urna pretium finibus. Aliquam pellentesque magna ipsum, vel sodales magna viverra non. Aliquam quis nisi gravida, feugiat leo in, tempus orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean non sagittis diam, sed tempor odio. Proin id tempus enim. Nullam eget vulputate erat. Vivamus sapien tellus, tincidunt at tellus sed, accumsan venenatis velit.";
 
 const answersArr = [a1, a2, a3];
+let backBtn = $("#back").click(function (e) { 
+    e.preventDefault();
+});
 
-function back() {
-    var duration = 3000;
-    var answer = document.getElementById("answer");
-    var backBtn = document.getElementById("back");
-    var paragraph = answer.childNodes[0];
-    answer.style.animationName = "hide";
-    answer.style.animationDuration = "3s";
-    backBtn.style.animationName = "hide";
-    backBtn.style.animationDuration = "3s";
-    setTimeout(hideHTMLElement, duration, answer);
-    setTimeout(hideHTMLElement, duration, backBtn);
-    setTimeout(removeChild, duration, paragraph);
-}
-
-function showAnswer(obj) {
-    var backBtn = $("#back");
-    var answerContainer = $("#answer");
-    var question = obj.id;
-    var answerString = getAnswer(question);
-    var paragraph = document.createElement("p");
-    var textNode = document.createTextNode(answerString);
-    var questionList = document.getElementById("question-list").children;
-    paragraph.appendChild(textNode);
-    answerContainer.appendChild(paragraph);
-     
-    answer.style.animationName = "show";
-    answer.style.animationDuration = "5s";
-    answer.style.visibility = "visible";
-    // backBtn.style.animationDelay = "2s";
-    backBtn.style.animationName = "show";
-    backBtn.style.animationDuration = "5s";
-    backBtn.style.visibility = "visible";
-}
-
-function getAnswer(question) {
-    switch (question) {
-        case "a1": 
-            return answersArr[0];
-        case "a2":
-            return answersArr[1];
-        case "a3":
-            return answersArr[2];    
-        default:
-            return undefined;    
-    }
-}
-
-function removeChild(element) {
-    if (element.hasChildNodes) {
-        element.remove();
-    }
-}
-
-function hideHTMLElement(element) {
-    element.style.visibility = "hidden";
-}
-
-function displayNoneHTMLElement(element) {
-    element.style.display = "none";
-}
-
-
-// 3d animated containers
 
 
